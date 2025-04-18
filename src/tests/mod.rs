10e510f04d6de3aee928d4d965134f00f3bdaa55
极速蜗牛 <31986081+Jisu-Woniu@users.noreply.github.com>
2025-04-18 21:27:00 +0800

    Add deps and update rust-version.

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -4,26 +4,34 @@ mod tree_utils;
 use crate::Solution;
 
 macro_rules! test_eq {
-    ($($(#[$m: meta])* $i:ident: $actual:expr, $expect:expr);+ $(;)?) => {
-        $(
-            #[test]
-            $(#[$m])*
-            fn $i() {
-                assert_eq!($actual, $expect);
-            }
-        )+
+    ($(#[$group_meta: meta])* $group_name:ident : $($(#[$fn_meta: meta])* $i:ident => $actual:expr, $expect:expr);+ $(;)?) => {
+        #[cfg(test)]
+        $(#[$group_meta])*
+        mod $group_name {
+            use super::*;
+
+            $(
+                #[test]
+                $(#[$fn_meta])*
+                fn $i() {
+                    assert_eq!($actual, $expect);
+                }
+            )+
+        }
     };
 }
 
 // Multiple cases in one invocation.
 test_eq! {
-    test1: Solution::dummy_fn(1), 1;
-    test2: Solution::dummy_fn(2), 2;
+    tests:
+    test1 => Solution::dummy_fn(1), 1;
+    test2 => Solution::dummy_fn(2), 2;
 }
 
 // Test with custom attributes
 test_eq!(
     #[cfg(any())]
+    failing_tests:
     #[ignore = "This test will fail"]
-    failing_test: Solution::dummy_fn(1), -1;
+    failing_test => Solution::dummy_fn(1), -1;
 );
